49d3585050a29e9ec1f0541a831245adf8f5250e
Cheesemooncake <72270829+Cheesemooncake@users.noreply.github.com>
2022-10-30 23:41:26 +0700

    Add files via upload

---
+++ 1_t/t/src/main/java/com/example/t/HelloController.java
@@ -0,0 +1,13 @@
+package com.example.t;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+
+public class HelloController {
+    @FXML
+    protected void onHelloButtonClick() {
+        while (true) {
+
+        }
+    }
+}
---
+++ 1_t/t/src/main/java/module-info.java
@@ -0,0 +1,8 @@
+module com.example.t {
+    requires javafx.controls;
+    requires javafx.fxml;
+
+
+    opens com.example.t to javafx.fxml;
+    exports com.example.t;
+}
